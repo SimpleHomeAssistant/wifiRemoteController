28093af6cd8a394d2da9ee78fcea99551a2208b7
jx wang <jx@x2v3.com>
2022-01-16 16:18:42 +0800

    make oled work

--- src/ControllerGateway/src/user_main.c
+++ src/ControllerGateway/src/user_main.c
@@ -1,31 +1,10 @@
-/*
- * ESPRSSIF MIT License
- *
- * Copyright (c) 2017 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
- *
- * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP8266 only, in which case,
- * it is free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
 
 #include "esp_common.h"
 #include "user_config.h"
+#include "driver/oled.h"
+#include "uart.h"
 
-static os_timer_t timer;
+
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -76,41 +55,9 @@ uint32 user_rf_cal_sector_set(void)
     return rf_cal_sec;
 }
 
-LOCAL void ICACHE_FLASH_ATTR wait_for_connection_ready(uint8 flag)
+void user_init(void * ignore)
 {
-    os_timer_disarm(&timer);
-    if(wifi_station_connected()){
-        os_printf("connected\n");
-    } else {
-        os_printf("reconnect after 2s\n");
-        os_timer_setfn(&timer, (os_timer_func_t *)wait_for_connection_ready, NULL);
-        os_timer_arm(&timer, 2000, 0);
-    }
-}
-
-LOCAL void ICACHE_FLASH_ATTR on_wifi_connect(){
-    os_timer_disarm(&timer);
-    os_timer_setfn(&timer, (os_timer_func_t *)wait_for_connection_ready, NULL);
-    os_timer_arm(&timer, 100, 0);
-}
-
-LOCAL void ICACHE_FLASH_ATTR on_wifi_disconnect(uint8_t reason){
-    os_printf("disconnect %d\n", reason);
-}
-
-/******************************************************************************
- * FunctionName : user_init
- * Description  : entry of user application, init user function here
- * Parameters   : none
- * Returns      : none
-*******************************************************************************/
-void user_init(void)
-{
-    printf("SDK version:%s\n", system_get_sdk_version());
-
-    set_on_station_connect(on_wifi_connect);
-    set_on_station_disconnect(on_wifi_disconnect);
-    init_esp_wifi();
-    stop_wifi_ap();
-    start_wifi_station(SSID, PASSWORD);
+    os_delay_us(10000);
+    OLED_Init();
+    OLED_ShowString( 0, 0, "Hello ESP8266!" );
 }
